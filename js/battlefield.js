4dd2dccaffa3bc1a0c5606926c8eb81784c00cc9
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-04-10 16:27:57 +0200

    changing my methodes into functions and restart from there

--- js/battlefield.js
+++ js/battlefield.js
@@ -1,3 +1,23 @@
+
+/**
+ * Return a random value between 0 and a chosen number.
+ * @param {number} max - chosen number
+ * @returns {number} - random value
+ */
+function getRandomValue(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+
+/**
+ * Gets a random value from an array
+ * @param {array} array of datas 
+ * @returns {*} - a random value
+ */
+function getRandomArrayValue(array) {
+    return array[getRandomValue(array.length - 1)];
+}
+
 const characters = [
     {
         name: 'Zangief',
@@ -5,18 +25,7 @@ const characters = [
         xp: 7,
         weapon: 9,
         shield: 8,
-        /**
-         * Get a random weapon force added to a fixed xp.
-         * @returns {number} A random attack number
-         */
-        attackForce: function(){
-            let weaponForce = 0;
-            return weaponForce = Math.floor(Math.random() * (this.weapon + 1)) + this.xp;
-        },
-        defenseForce: function(){
-            let shieldForce = 0;
-            return shieldForce = Math.floor(Math.random() * (this.shield + 1)) + this.xp;
-        }
+        alive : true
     },
     {
         name: 'Chun-Li',
@@ -24,6 +33,7 @@ const characters = [
         xp: 7,
         weapon: 8,
         shield: 9,
+        alive : true
     },
     {
         name: 'Blanca',
@@ -31,38 +41,33 @@ const characters = [
         xp: 4,
         weapon: 3,
         shield: 6,
+        alive : true
     },
     {
         name: 'Ken',
         life: 50,
         xp: 4,
-        weapon: 3,
+        weapon: 6,
         shield: 3,
+        alive : true
     }
 ];
 
-console.table(characters);
-console.log(characters[0]);
-
-
-function randomAttackForce(weapon){
-    let weaponForce = 0;
-    return weaponForce = Math.floor(Math.random() * (weapon + 1));
-}
-console.log(`${characters[0].name} frappe son adversaire et lui inflige ${characters[0].attackForce()} dégâts.`);
-console.log(`${characters[0].name} se fait prend un coup. Il a une défense de ${characters[0].defenseForce()}.`)
-
 /**
- * Get a random value from an array using its indexes.
- * @param {array} array The array you want a random value from.
- * @returns {number} The random value.
+ * Get random attack score from character stats
+ * @param {object} attacker - An object representing a character
+ * @returns {number} - Random attack score
  */
-function getRandomValueArray(array) {
-    return array[Math.floor(Math.random() * (array.length))]
+function getAttackScore(attacker) {
+    return getRandomValue(attacker.weapon) + attacker.xp;
 }
 
-function attackOpponent(array) {
+console.table(characters);
 
-    let attacker = getRandomValueArray(array);
-    let defender = getRandomValueArray(array);
-}
+const attacker = getRandomArrayValue(characters);
+// const defender;
+
+console.log(
+    attacker,
+    getAttackScore(attacker)
+);

9abf33c37fc478868c01617b61a4ee7c10f8e3c2
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-04-10 12:07:41 +0200

    newbie exercices corrected

---
+++ js/battlefield.js
@@ -0,0 +1,30 @@
+const characters = {
+    Godzilla: {
+        life: 50,
+        xp: 5,
+        weapon: 9,
+        shield: 8
+    },
+    Kong: {
+        life: 50,
+        xp: 7,
+        weapon: 8,
+        shield: 9,
+    },
+    Hulk: {
+        life: 50,
+        xp: 4,
+        weapon: 3,
+        shield: 6,
+    },
+    Aquaman: {
+        life: 50,
+        xp: 4,
+        weapon: 3,
+        shield: 3,
+    }
+};
+
+function attackOpponent(object){
+
+}
